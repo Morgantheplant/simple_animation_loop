3a389d4d04d450747b69f9e3662d216ae8e18b75
Morgan Plant <morgantheplant@gmail.com>
2017-02-19 11:43:25 -0800

    add tests

--- animationLoop.test.js
+++ animationLoop.test.js
@@ -43,7 +43,7 @@ test('Animation loop should keep track of time', function(done){
 });
 
 
-test('AnimationLoop should add animations and call them appropriately', function(){
+test('Animation Loop should add animations and call them appropriately', function(){
   var animation = jest.fn(),
   animation2 = jest.fn(),
   calls, calls2, done = false;
@@ -80,7 +80,97 @@ test('AnimationLoop should add animations and call them appropriately', function
   expect(calls2).toBe(animation2.mock.calls.length);
 });
 
+test('Animation loop should only add functions', function(){
+  var animation = jest.fn();
+  // make sure it starts empty
+  expect(animationLoop.animations.length).toBe(0);
+  animationLoop.addAnimation(animation);
+  // make sure one animation is added
+  expect(animationLoop.animations.length).toBe(1);
+  // try to add non function values
+  ['string', {}, null, undefined, 23, false, NaN].forEach(function(type){
+    animationLoop.addAnimation(type);
+  });
+  // make sure no animations are added
+  expect(animationLoop.animations.length).toBe(1);
+});
 
+
+test('Animation loop should remove animations correctly', function(){
+  var animation = jest.fn(),
+  animation2 = jest.fn(),
+  animation3 = jest.fn();
+  animationLoop.start();
+  // make sure it starts empty
+  expect(animationLoop.animations.length).toBe(0);
+  animationLoop.addAnimation(animation);
+  // make sure one animation is added
+  expect(animationLoop.animations.length).toBe(1);
+  animationLoop.addAnimation(animation2);
+  // make sure two animations are added
+  expect(animationLoop.animations.length).toBe(2);
+  animationLoop.addAnimation(animation3);
+  // make sure three animations are added
+  expect(animationLoop.animations.length).toBe(3);
+  jest.runTimersToTime(1000);
+  animationLoop.removeAnimation(animation2);
+  // make sure animation2 is removed
+  expect(animationLoop.animations.length).toBe(2);
+  expect(animationLoop.animations.indexOf(animation2)).toBe(-1);
+  expect(animationLoop.animations.indexOf(animation)).not.toBe(-1);
+  expect(animationLoop.animations.indexOf(animation3)).not.toBe(-1);
+  // make sure first animation is removed
+  animationLoop.removeAnimation(animation);
+  expect(animationLoop.animations.length).toBe(1);
+  expect(animationLoop.animations.indexOf(animation)).toBe(-1);
+  expect(animationLoop.animations.indexOf(animation3)).not.toBe(-1);
+  // try removing animation 2 again (should do nothing)
+  animationLoop.removeAnimation(animation2);
+  expect(animationLoop.animations.length).toBe(1);
+  // remove last animation
+  animationLoop.removeAnimation(animation3);
+  expect(animationLoop.animations.length).toBe(0);
+  expect(animationLoop.animations.indexOf(animation3)).toBe(-1);
+  // store values
+  var calls1 = animation.mock.calls.length;
+  var calls2 = animation2.mock.calls.length;
+  var calls3 = animation3.mock.calls.length;
+  // make sure all animations were called
+  expect(calls1).toBeGreaterThan(0)
+  expect(calls2).toBeGreaterThan(0)
+  expect(calls3).toBeGreaterThan(0)
+
+});
+
+test('setAnimationTimeout should mimic setTimeout functionality', function(){
+  var timedAnimation = jest.fn();
+  animationLoop.start();
+  // make sure it starts empty
+  expect(animationLoop.animations.length).toBe(0);
+  animationLoop.setAnimationTimeout(timedAnimation, 2000);
+  jest.runTimersToTime(4000);
+  // animation should only be called once
+  expect(timedAnimation.mock.calls.length).toBe(1);
+  expect(animationLoop.animations.length).toBe(0);
+});
+
+test('setAnimationInterval should mimic setInterval functionality', function(){
+  var timedAnimation = jest.fn();
+  // animationLoop.start();
+  // animationLoop.setAnimationInterval(timedAnimation, 1000);
+  // jest.runTimersToTime(1000);
+  // expect(timedAnimation.mock.calls.length).toBe(1);
+  // expect(animationLoop.animations.length).toBe(1);
+  // jest.runTimersToTime(2500);
+  // expect(timedAnimation.mock.calls.length).toBe(2);
+  // expect(animationLoop.animations.length).toBe(1);
+  // jest.runTimersToTime(3500);
+  // expect(timedAnimation.mock.calls.length).toBe(3);
+  // expect(animationLoop.animations.length).toBe(1);
+  // jest.runTimersToTime(3600);
+  // expect(timedAnimation.mock.calls.length).toBe(3);
+  // expect(animationLoop.animations.length).toBe(1);
+});
 
 
 
